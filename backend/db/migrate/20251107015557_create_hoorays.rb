5b936ef8b7ca69341fdb9c27d9aee73bf477b3d5
chobby <aa.ing85@gmail.com>
2025-11-07 11:03:53 +0900

    feat: complete initial migration setup

---
+++ backend/db/migrate/20251107015557_create_hoorays.rb
@@ -0,0 +1,12 @@
+class CreateHoorays < ActiveRecord::Migration[8.1]
+  def change
+    create_table :hoorays do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :post, null: false, foreign_key: true
+
+      t.timestamps
+    end
+
+    add_index :hoorays, [:user_id, :post_id], unique: true
+  end
+end
