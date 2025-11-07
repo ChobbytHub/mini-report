5c9657b4a7c6de157e74197dff1f32fcb73bfed6
chobby <aa.ing85@gmail.com>
2025-11-07 13:40:06 +0900

    fix users_controller.rb

--- backend/app/controllers/api/v1/users/users_controller.rb
+++ backend/app/controllers/api/v1/users/users_controller.rb
@@ -1,10 +1,12 @@
 module Api
   module V1
-    class UsersController < ApplicationController
-      before_action :authenticate_user!
+    module Users
+      class UsersController < ApplicationController
+        before_action :authenticate_user!
 
-      def me
-        render json: current_user, status: :ok
+        def me
+          render json: current_user, status: :ok
+        end
       end
     end
   end
